dd78a4c0092b8c04f156e0cc88e98c62cf3485b2
anngelicafernanda <devanngelc@gmail.com>
2022-12-15 15:19:12 -0300

    Lista de Pacientes

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,27 @@
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+    <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {" "}
+    <span className="font-normal normal-case">Hook</span>
+    </p>
+
+    <p className="font-bold mb-3 text-gray-700 uppercase">Nombre Propietario: {" "}
+    <span className="font-normal normal-case">Angelica</span>
+    </p>
+
+    <p className="font-bold mb-3 text-gray-700 uppercase">Email: {" "}
+    <span className="font-normal normal-case">correo@correo.com</span>
+    </p>
+
+    <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {" "}
+    <span className="font-normal normal-case">10 Diciembre de 2022</span>
+    </p>
+
+    <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {" "}
+    <span className="font-normal normal-case">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque adipisci quibusdam vitae cum est modi ullam quas. Optio perspiciatis reiciendis obcaecati ducimus dicta architecto ipsam quidem! Facilis cumque voluptatum assumenda?</span>
+    </p>
+</div>
+  )
+}
+
+export default Paciente
