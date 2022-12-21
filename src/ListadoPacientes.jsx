8bb5c4691cd854db0e2337a6de3c124cd0a4bef0
anngelicafernanda <devanngelc@gmail.com>
2022-12-21 15:09:02 -0300

    Ajuste de textos

--- src/ListadoPacientes.jsx
+++ src/ListadoPacientes.jsx
@@ -32,7 +32,7 @@ const ListadoPacientes = ({ pacientes, setPaciente, eliminarPaciente }) => {
         <>
           <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
           <p className="text-xl mt-5 mb-10 text-center">
-           Acomienza agregando pacientes {" "}
+           Comienza agregando pacientes {" "}
             <span className="text-indigo-600 font-bold">
               y apareceran en este lugar
             </span>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
 
     return (
       <div>
-        <h1 className="font-black text-5xl text-center w-2/3 mx-auto ">Seguimientos Pacientes {" "}<span className="text-indigo-600">Veterinaria</span></h1>
+        <h1 className="font-black text-5xl text-center w-2/3 mx-auto ">Seguimiento de Pacientes {" "} <br></br><span className="text-indigo-600">Clinica Veterinaria Orion</span></h1>
        
         </div>
         ) 
